b56377bdc880ef3bb7eaf335f656db8f903b7fff
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-05 00:17:52 +0900

    Header, Sidebar

---
+++ frontend/src/pages/CoordinateMapPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class MapPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>This is the Map page</p>;
+  }
+}
