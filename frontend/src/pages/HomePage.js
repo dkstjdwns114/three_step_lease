a810a623aa40d8a10aba6cf76e06a5532e32b07f
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-05 03:36:15 +0900

    Home 껍데기 추가 (차트 나오게 해야됨)

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PageHeader from "../components/Navigation/PageHeader";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -6,6 +7,129 @@ export default class HomePage extends Component {
   }
 
   render() {
-    return <p>This is the home page</p>;
+    return (
+      <>
+        <PageHeader headerTitle={"메인 페이지"} />
+        {/* Content area */}
+        <div class="content">
+          {/* Pies */}
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card">
+                <div class="card-header header-elements-inline">
+                  <h5 class="card-title">Basic donut chart</h5>
+                </div>
+
+                <div class="card-body">
+                  <div class="chart-container text-center">
+                    <div class="d-inline-block" id="c3-donut-chart"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="card">
+                <div class="card-header header-elements-inline">
+                  <h5 class="card-title">Basic donut chart</h5>
+                </div>
+
+                <div class="card-body">
+                  <div class="chart-container text-center">
+                    <div class="d-inline-block" id="c3-donut-chart"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* /pies */}
+
+          {/* Bar chart */}
+          <div class="card">
+            <div class="card-header header-elements-inline">
+              <h5 class="card-title">Basic bar chart</h5>
+              <div class="header-elements">
+                <div class="list-icons">
+                  <a class="list-icons-item" data-action="collapse"></a>
+                  <a class="list-icons-item" data-action="reload"></a>
+                  <a class="list-icons-item" data-action="remove"></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="card-body">
+              <div class="chart-container">
+                <div class="chart" id="c3-bar-chart"></div>
+              </div>
+            </div>
+          </div>
+          {/* /bar chart */}
+
+          {/* Stacked bar chart */}
+          <div class="card">
+            <div class="card-header header-elements-inline">
+              <h5 class="card-title">Stacked bar chart</h5>
+              <div class="header-elements">
+                <div class="list-icons">
+                  <a class="list-icons-item" data-action="collapse"></a>
+                  <a class="list-icons-item" data-action="reload"></a>
+                  <a class="list-icons-item" data-action="remove"></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="card-body">
+              <div class="chart-container">
+                <div class="chart" id="c3-bar-stacked-chart"></div>
+              </div>
+            </div>
+          </div>
+          {/* /stacked bar chart */}
+
+          {/* Combined chart */}
+          <div class="card">
+            <div class="card-header header-elements-inline">
+              <h5 class="card-title">Combination of charts</h5>
+              <div class="header-elements">
+                <div class="list-icons">
+                  <a class="list-icons-item" data-action="collapse"></a>
+                  <a class="list-icons-item" data-action="reload"></a>
+                  <a class="list-icons-item" data-action="remove"></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="card-body">
+              <div class="chart-container">
+                <div class="chart" id="c3-combined-chart"></div>
+              </div>
+            </div>
+          </div>
+          {/* /combined chart */}
+
+          {/* Scatter plot */}
+          <div class="card">
+            <div class="card-header header-elements-inline">
+              <h5 class="card-title">Scatter plot</h5>
+              <div class="header-elements">
+                <div class="list-icons">
+                  <a class="list-icons-item" data-action="collapse"></a>
+                  <a class="list-icons-item" data-action="reload"></a>
+                  <a class="list-icons-item" data-action="remove"></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="card-body">
+              <div class="chart-container">
+                <div class="chart" id="c3-scatter-chart"></div>
+              </div>
+            </div>
+          </div>
+          {/* /scatter plot  */}
+        </div>
+        {/* /content area  */}
+      </>
+    );
   }
 }
