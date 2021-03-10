5d0b1f9c2f725f3db31d07fcce70677ff79ed19b
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-11 02:28:45 +0900

    class to className

--- frontend/src/components/Navigation/PageHeader.js
+++ frontend/src/components/Navigation/PageHeader.js
@@ -5,32 +5,35 @@ const pageHeader = (props) => {
   return (
     <>
       {/* Page header */}
-      <div class="page-header page-header-light">
-        <div class="page-header-content header-elements-md-inline">
-          <div class="page-title d-flex">
+      <div className="page-header page-header-light">
+        <div className="page-header-content header-elements-md-inline">
+          <div className="page-title d-flex">
             <h4>
-              <i class="icon-arrow-left52 mr-2"></i> {props.headerTitle}
+              <i className="icon-arrow-left52 mr-2"></i> {props.headerTitle}
             </h4>
           </div>
         </div>
 
-        <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
-          <div class="d-flex">
-            <div class="breadcrumb">
-              <Link to="/" class="breadcrumb-item">
-                <i class="icon-home2 mr-2"></i> Home
+        <div className="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+          <div className="d-flex">
+            <div className="breadcrumb">
+              <Link to="/" className="breadcrumb-item">
+                <i className="icon-home2 mr-2"></i> Home
               </Link>
             </div>
 
-            <Link to="#" class="header-elements-toggle text-default d-md-none">
-              <i class="icon-more"></i>
+            <Link
+              to="#"
+              className="header-elements-toggle text-default d-md-none"
+            >
+              <i className="icon-more"></i>
             </Link>
           </div>
 
-          <div class="header-elements d-none">
-            <div class="breadcrumb justify-content-center">
-              <Link to="#" class="breadcrumb-elements-item">
-                <i class="icon-comment-discussion mr-2"></i>
+          <div className="header-elements d-none">
+            <div className="breadcrumb justify-content-center">
+              <Link to="#" className="breadcrumb-elements-item">
+                <i className="icon-comment-discussion mr-2"></i>
                 Support
               </Link>
             </div>
