4a7733e8bede78e0a9823b370a5b7228b9ad022f
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-11 02:28:25 +0900

    불필요한 코드 삭제

--- frontend/src/components/Navigation/PageHeader.js
+++ frontend/src/components/Navigation/PageHeader.js
@@ -11,33 +11,13 @@ const pageHeader = (props) => {
             <h4>
               <i class="icon-arrow-left52 mr-2"></i> {props.headerTitle}
             </h4>
-            <Link to="#" class="header-elements-toggle text-default d-md-none">
-              <i class="icon-more"></i>
-            </Link>
           </div>
-
-          {/* <div class="header-elements d-none">
-            <div class="d-flex justify-content-center">
-              <Link to="#" class="btn btn-link btn-float text-default">
-                <i class="icon-bars-alt text-primary"></i>
-                <span>Statistics</span>
-              </Link>
-              <Link to="#" class="btn btn-link btn-float text-default">
-                <i class="icon-calculator text-primary"></i>{" "}
-                <span>Invoices</span>
-              </Link>
-              <Link to="#" class="btn btn-link btn-float text-default">
-                <i class="icon-calendar5 text-primary"></i>{" "}
-                <span>Schedule</span>
-              </Link>
-            </div>
-          </div> */}
         </div>
 
         <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
           <div class="d-flex">
             <div class="breadcrumb">
-              <Link to="#" class="breadcrumb-item">
+              <Link to="/" class="breadcrumb-item">
                 <i class="icon-home2 mr-2"></i> Home
               </Link>
             </div>
