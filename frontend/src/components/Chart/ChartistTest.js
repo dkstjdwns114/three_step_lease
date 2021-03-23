1813ae6f2c19fea15228be41b31c1bc5143eba17
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-23 22:32:49 +0900

    chartist animation chart 2개로 나누기

--- frontend/src/components/Chart/ChartistTest.js
+++ frontend/src/components/Chart/ChartistTest.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
-import ChartistGraph from "react-chartist";
+import React, { useEffect } from "react";
 
 const ChartistTest = (props) => {
   useEffect(() => {
     let Chartist = require("chartist");
-    var chart = new Chartist.Line(
-      ".ct-chart",
+    // chart1
+    let chart1 = new Chartist.Line(
+      ".ct-chart1",
       {
-        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+        labels: ["1", "2", "3", "4", "5", "6"],
         series: [
-          [12, 9, 7, 8, 5, 4, 6, 2, 3, 3, 4, 6],
-          [4, 5, 3, 7, 3, 5, 5, 3, 4, 4, 5, 5],
-          [5, 3, 4, 5, 6, 3, 3, 4, 5, 6, 3, 4],
-          [3, 4, 5, 6, 7, 6, 4, 5, 6, 7, 6, 3]
+          [12, 9, 7, 8, 5, 4],
+          [4, 5, 3, 7, 3, 5],
+          [5, 3, 4, 5, 6, 3],
+          [3, 4, 5, 6, 7, 6]
         ]
       },
       {
@@ -20,50 +20,41 @@ const ChartistTest = (props) => {
       }
     );
 
-    // Let's put a sequence number aside so we can use it in the event callbacks
-    var seq = 0,
-      delays = 80,
-      durations = 500;
+    let seq1 = 0;
+    let delays1 = 100;
+    let durations1 = 500;
 
-    // Once the chart is fully created we reset the sequence
-    chart.on("created", function () {
-      seq = 0;
+    chart1.on("created", function () {
+      seq1 = 0;
     });
 
-    // On each drawn element by Chartist we use the Chartist.Svg API to trigger SMIL animations
-    chart.on("draw", function (data) {
-      seq++;
+    chart1.on("draw", function (data) {
+      seq1++;
 
       if (data.type === "line") {
-        // If the drawn element is a line we do a simple opacity fade in. This could also be achieved using CSS3 animations.
         data.element.animate({
           opacity: {
-            // The delay when we like to start the animation
-            begin: seq * delays + 1000,
-            // Duration of the animation
-            dur: durations,
-            // The value where the animation should start
+            begin: seq1 * delays1 + 2000,
+            dur: durations1,
             from: 0,
-            // The value where it should end
             to: 1
           }
         });
       } else if (data.type === "label" && data.axis === "x") {
         data.element.animate({
           y: {
-            begin: seq * delays,
-            dur: durations,
+            begin: seq1 * delays1,
+            dur: durations1,
             from: data.y + 100,
             to: data.y,
-            // We can specify an easing function from Chartist.Svg.Easing
             easing: "easeOutQuart"
           }
         });
       } else if (data.type === "label" && data.axis === "y") {
         data.element.animate({
           x: {
-            begin: seq * delays,
-            dur: durations,
+            begin: seq1 * delays1,
+            dur: durations1,
             from: data.x - 100,
             to: data.x,
             easing: "easeOutQuart"
@@ -72,51 +63,50 @@ const ChartistTest = (props) => {
       } else if (data.type === "point") {
         data.element.animate({
           x1: {
-            begin: seq * delays,
-            dur: durations,
+            begin: seq1 * delays1,
+            dur: durations1,
             from: data.x - 10,
             to: data.x,
             easing: "easeOutQuart"
           },
           x2: {
-            begin: seq * delays,
-            dur: durations,
+            begin: seq1 * delays1,
+            dur: durations1,
             from: data.x - 10,
             to: data.x,
             easing: "easeOutQuart"
           },
           opacity: {
-            begin: seq * delays,
-            dur: durations,
+            begin: seq1 * delays1,
+            dur: durations1,
             from: 0,
             to: 1,
             easing: "easeOutQuart"
           }
         });
       } else if (data.type === "grid") {
-        // Using data.axis we get x or y which we can use to construct our animation definition objects
-        var pos1Animation = {
-          begin: seq * delays,
-          dur: durations,
+        let pos1Animation = {
+          begin: seq1 * delays1,
+          dur: durations1,
           from: data[data.axis.units.pos + "1"] - 30,
           to: data[data.axis.units.pos + "1"],
           easing: "easeOutQuart"
         };
 
-        var pos2Animation = {
-          begin: seq * delays,
-          dur: durations,
+        let pos2Animation = {
+          begin: seq1 * delays1,
+          dur: durations1,
           from: data[data.axis.units.pos + "2"] - 100,
           to: data[data.axis.units.pos + "2"],
           easing: "easeOutQuart"
         };
 
-        var animations = {};
+        let animations = {};
         animations[data.axis.units.pos + "1"] = pos1Animation;
         animations[data.axis.units.pos + "2"] = pos2Animation;
         animations["opacity"] = {
-          begin: seq * delays,
-          dur: durations,
+          begin: seq1 * delays1,
+          dur: durations1,
           from: 0,
           to: 1,
           easing: "easeOutQuart"
@@ -126,14 +116,138 @@ const ChartistTest = (props) => {
       }
     });
 
-    // For the sake of the example we update the chart every time it's created with a delay of 10 seconds
-    chart.on("created", function () {
+    chart1.on("created", function () {
       if (window.__exampleAnimateTimeout) {
         clearTimeout(window.__exampleAnimateTimeout);
         window.__exampleAnimateTimeout = null;
       }
       window.__exampleAnimateTimeout = setTimeout(
-        chart.update.bind(chart),
+        chart1.update.bind(chart1),
+        30000
+      );
+    });
+
+    // chart2
+
+    let chart2 = new Chartist.Line(
+      ".ct-chart2",
+      {
+        labels: ["7", "8", "9", "10", "11", "12"],
+        series: [
+          [5, 3, 4, 5, 6, 3],
+          [4, 5, 3, 7, 3, 5],
+          [12, 9, 7, 8, 5, 4],
+          [3, 4, 5, 6, 7, 6]
+        ]
+      },
+      {
+        low: 0
+      }
+    );
+
+    let seq2 = 0;
+    let delays2 = 100;
+    let durations2 = 500;
+
+    chart2.on("created", function () {
+      seq2 = 0;
+    });
+
+    chart2.on("draw", function (data) {
+      seq2++;
+
+      if (data.type === "line") {
+        data.element.animate({
+          opacity: {
+            begin: seq2 * delays2 + 2000,
+            dur: durations2,
+            from: 0,
+            to: 1
+          }
+        });
+      } else if (data.type === "label" && data.axis === "x") {
+        data.element.animate({
+          y: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: data.y + 100,
+            to: data.y,
+            easing: "easeOutQuart"
+          }
+        });
+      } else if (data.type === "label" && data.axis === "y") {
+        data.element.animate({
+          x: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: data.x - 100,
+            to: data.x,
+            easing: "easeOutQuart"
+          }
+        });
+      } else if (data.type === "point") {
+        data.element.animate({
+          x1: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: data.x - 10,
+            to: data.x,
+            easing: "easeOutQuart"
+          },
+          x2: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: data.x - 10,
+            to: data.x,
+            easing: "easeOutQuart"
+          },
+          opacity: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: "easeOutQuart"
+          }
+        });
+      } else if (data.type === "grid") {
+        let pos1Animation = {
+          begin: seq2 * delays2,
+          dur: durations2,
+          from: data[data.axis.units.pos + "1"] - 30,
+          to: data[data.axis.units.pos + "1"],
+          easing: "easeOutQuart"
+        };
+
+        let pos2Animation = {
+          begin: seq2 * delays2,
+          dur: durations2,
+          from: data[data.axis.units.pos + "2"] - 100,
+          to: data[data.axis.units.pos + "2"],
+          easing: "easeOutQuart"
+        };
+
+        let animations = {};
+        animations[data.axis.units.pos + "1"] = pos1Animation;
+        animations[data.axis.units.pos + "2"] = pos2Animation;
+        animations["opacity"] = {
+          begin: seq2 * delays2,
+          dur: durations2,
+          from: 0,
+          to: 1,
+          easing: "easeOutQuart"
+        };
+
+        data.element.animate(animations);
+      }
+    });
+
+    chart2.on("created", function () {
+      if (window.__exampleAnimateTimeout) {
+        clearTimeout(window.__exampleAnimateTimeout);
+        window.__exampleAnimateTimeout = null;
+      }
+      window.__exampleAnimateTimeout = setTimeout(
+        chart2.update.bind(chart2),
         30000
       );
     });
@@ -142,16 +256,19 @@ const ChartistTest = (props) => {
   return (
     <>
       {/* <!-- Panel line css animation Chart --> */}
-      <div class="panel">
-        <div class="panel-heading">
-          <h3 class="panel-title">CSS ANIMATION</h3>
+      <div className="panel">
+        <div className="panel-heading">
+          <h3 className="panel-title">CSS ANIMATION</h3>
         </div>
-        <div class="panel-body">
-          {/* <div className="row row-lg"> */}
-          {/* <div className="col-lg-6"> */}
-          <div className="ct-chart" style={{ height: "400px" }}></div>
-          {/* </div> */}
-          {/* </div> */}
+        <div className="panel-body">
+          <div className="row row-lg">
+            <div className="col-lg-6">
+              <div className="ct-chart1" style={{ height: "300px" }}></div>
+            </div>
+            <div className="col-lg-6">
+              <div className="ct-chart2" style={{ height: "300px" }}></div>
+            </div>
+          </div>
         </div>
       </div>
       {/* <!-- End Panel Pie --> */}
