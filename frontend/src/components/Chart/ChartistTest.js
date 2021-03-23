da3508e42891f560c2b4556e02db101154ba01b2
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-24 02:06:25 +0900

    월별 Line 차트 틀 완료

--- frontend/src/components/Chart/ChartistTest.js
+++ frontend/src/components/Chart/ChartistTest.js
@@ -3,270 +3,205 @@ import React, { useEffect } from "react";
 const ChartistTest = (props) => {
   useEffect(() => {
     let Chartist = require("chartist");
-    // chart1
-    let chart1 = new Chartist.Line(
-      ".ct-chart1",
-      {
-        labels: ["1", "2", "3", "4", "5", "6"],
-        series: [
-          [12, 9, 7, 8, 5, 4],
-          [4, 5, 3, 7, 3, 5],
-          [5, 3, 4, 5, 6, 3],
-          [3, 4, 5, 6, 7, 6]
-        ]
-      },
-      {
-        low: 0
-      }
-    );
 
-    let seq1 = 0;
-    let delays1 = 100;
-    let durations1 = 500;
-
-    chart1.on("created", function () {
-      seq1 = 0;
-    });
-
-    chart1.on("draw", function (data) {
-      seq1++;
-
-      if (data.type === "line") {
-        data.element.animate({
-          opacity: {
-            begin: seq1 * delays1 + 2000,
-            dur: durations1,
-            from: 0,
-            to: 1
-          }
-        });
-      } else if (data.type === "label" && data.axis === "x") {
-        data.element.animate({
-          y: {
-            begin: seq1 * delays1,
-            dur: durations1,
-            from: data.y + 100,
-            to: data.y,
-            easing: "easeOutQuart"
-          }
-        });
-      } else if (data.type === "label" && data.axis === "y") {
-        data.element.animate({
-          x: {
-            begin: seq1 * delays1,
-            dur: durations1,
-            from: data.x - 100,
-            to: data.x,
-            easing: "easeOutQuart"
-          }
-        });
-      } else if (data.type === "point") {
-        data.element.animate({
-          x1: {
-            begin: seq1 * delays1,
-            dur: durations1,
-            from: data.x - 10,
-            to: data.x,
-            easing: "easeOutQuart"
-          },
-          x2: {
-            begin: seq1 * delays1,
-            dur: durations1,
-            from: data.x - 10,
-            to: data.x,
-            easing: "easeOutQuart"
-          },
-          opacity: {
-            begin: seq1 * delays1,
-            dur: durations1,
-            from: 0,
-            to: 1,
-            easing: "easeOutQuart"
-          }
-        });
-      } else if (data.type === "grid") {
-        let pos1Animation = {
-          begin: seq1 * delays1,
-          dur: durations1,
-          from: data[data.axis.units.pos + "1"] - 30,
-          to: data[data.axis.units.pos + "1"],
-          easing: "easeOutQuart"
-        };
-
-        let pos2Animation = {
-          begin: seq1 * delays1,
-          dur: durations1,
-          from: data[data.axis.units.pos + "2"] - 100,
-          to: data[data.axis.units.pos + "2"],
-          easing: "easeOutQuart"
-        };
-
-        let animations = {};
-        animations[data.axis.units.pos + "1"] = pos1Animation;
-        animations[data.axis.units.pos + "2"] = pos2Animation;
-        animations["opacity"] = {
-          begin: seq1 * delays1,
-          dur: durations1,
-          from: 0,
-          to: 1,
-          easing: "easeOutQuart"
-        };
-
-        data.element.animate(animations);
-      }
-    });
-
-    chart1.on("created", function () {
-      if (window.__exampleAnimateTimeout) {
-        clearTimeout(window.__exampleAnimateTimeout);
-        window.__exampleAnimateTimeout = null;
-      }
-      window.__exampleAnimateTimeout = setTimeout(
-        chart1.update.bind(chart1),
-        30000
-      );
-    });
-
-    // chart2
-
-    let chart2 = new Chartist.Line(
-      ".ct-chart2",
-      {
-        labels: ["7", "8", "9", "10", "11", "12"],
-        series: [
-          [5, 3, 4, 5, 6, 3],
-          [4, 5, 3, 7, 3, 5],
-          [12, 9, 7, 8, 5, 4],
-          [3, 4, 5, 6, 7, 6]
-        ]
-      },
-      {
-        low: 0
-      }
-    );
-
-    let seq2 = 0;
-    let delays2 = 100;
-    let durations2 = 500;
-
-    chart2.on("created", function () {
-      seq2 = 0;
-    });
-
-    chart2.on("draw", function (data) {
-      seq2++;
-
-      if (data.type === "line") {
-        data.element.animate({
-          opacity: {
-            begin: seq2 * delays2 + 2000,
-            dur: durations2,
-            from: 0,
-            to: 1
-          }
-        });
-      } else if (data.type === "label" && data.axis === "x") {
-        data.element.animate({
-          y: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: data.y + 100,
-            to: data.y,
-            easing: "easeOutQuart"
-          }
-        });
-      } else if (data.type === "label" && data.axis === "y") {
-        data.element.animate({
-          x: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: data.x - 100,
-            to: data.x,
-            easing: "easeOutQuart"
-          }
-        });
-      } else if (data.type === "point") {
-        data.element.animate({
-          x1: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: data.x - 10,
-            to: data.x,
-            easing: "easeOutQuart"
-          },
-          x2: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: data.x - 10,
-            to: data.x,
-            easing: "easeOutQuart"
-          },
-          opacity: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: 0,
-            to: 1,
-            easing: "easeOutQuart"
-          }
-        });
-      } else if (data.type === "grid") {
-        let pos1Animation = {
-          begin: seq2 * delays2,
-          dur: durations2,
-          from: data[data.axis.units.pos + "1"] - 30,
-          to: data[data.axis.units.pos + "1"],
-          easing: "easeOutQuart"
-        };
-
-        let pos2Animation = {
-          begin: seq2 * delays2,
-          dur: durations2,
-          from: data[data.axis.units.pos + "2"] - 100,
-          to: data[data.axis.units.pos + "2"],
-          easing: "easeOutQuart"
-        };
-
-        let animations = {};
-        animations[data.axis.units.pos + "1"] = pos1Animation;
-        animations[data.axis.units.pos + "2"] = pos2Animation;
-        animations["opacity"] = {
-          begin: seq2 * delays2,
-          dur: durations2,
-          from: 0,
-          to: 1,
-          easing: "easeOutQuart"
-        };
-
-        data.element.animate(animations);
-      }
-    });
-
-    chart2.on("created", function () {
-      if (window.__exampleAnimateTimeout) {
-        clearTimeout(window.__exampleAnimateTimeout);
-        window.__exampleAnimateTimeout = null;
-      }
-      window.__exampleAnimateTimeout = setTimeout(
-        chart2.update.bind(chart2),
-        30000
-      );
-    });
+    animationChart(Chartist, ".ct-chart1");
+    animationChart(Chartist, ".ct-chart2");
   }, []);
+
+  const animationChart = (Chartist, chartClassName) => {
+    let labelArray = [];
+    chartClassName === ".ct-chart1" &&
+      (labelArray = ["Jan", "2", "3", "4", "5", "6"]);
+    chartClassName === ".ct-chart2" &&
+      (labelArray = ["7", "8", "9", "10", "11", "12"]);
+
+    let chart = new Chartist.Line(
+      chartClassName,
+      {
+        labels: labelArray,
+        series: [
+          [12, 9, 7, 8, 5, 4],
+          [4, 5, 3, 7, 3, 5],
+          [5, 3, 4, 5, 6, 3],
+          [3, 4, 5, 6, 7, 6]
+        ]
+      },
+      {
+        low: 0
+      }
+    );
+
+    let seq = 0;
+    let delays = 100;
+    let durations = 500;
+
+    chart.on("created", function () {
+      seq = 0;
+    });
+
+    chart.on("draw", function (data) {
+      seq++;
+
+      if (data.type === "line") {
+        data.element.animate({
+          opacity: {
+            begin: seq * delays + 2000,
+            dur: durations,
+            from: 0,
+            to: 1
+          }
+        });
+      } else if (data.type === "label" && data.axis === "x") {
+        data.element.animate({
+          y: {
+            begin: seq * delays,
+            dur: durations,
+            from: data.y + 100,
+            to: data.y,
+            easing: "easeOutQuart"
+          }
+        });
+      } else if (data.type === "label" && data.axis === "y") {
+        data.element.animate({
+          x: {
+            begin: seq * delays,
+            dur: durations,
+            from: data.x - 100,
+            to: data.x,
+            easing: "easeOutQuart"
+          }
+        });
+      } else if (data.type === "point") {
+        data.element.animate({
+          x1: {
+            begin: seq * delays,
+            dur: durations,
+            from: data.x - 10,
+            to: data.x,
+            easing: "easeOutQuart"
+          },
+          x2: {
+            begin: seq * delays,
+            dur: durations,
+            from: data.x - 10,
+            to: data.x,
+            easing: "easeOutQuart"
+          },
+          opacity: {
+            begin: seq * delays,
+            dur: durations,
+            from: 0,
+            to: 1,
+            easing: "easeOutQuart"
+          }
+        });
+      } else if (data.type === "grid") {
+        let pos1Animation = {
+          begin: seq * delays,
+          dur: durations,
+          from: data[data.axis.units.pos + "1"] - 30,
+          to: data[data.axis.units.pos + "1"],
+          easing: "easeOutQuart"
+        };
+
+        let pos2Animation = {
+          begin: seq * delays,
+          dur: durations,
+          from: data[data.axis.units.pos + "2"] - 100,
+          to: data[data.axis.units.pos + "2"],
+          easing: "easeOutQuart"
+        };
+
+        let animations = {};
+        animations[data.axis.units.pos + "1"] = pos1Animation;
+        animations[data.axis.units.pos + "2"] = pos2Animation;
+        animations["opacity"] = {
+          begin: seq * delays,
+          dur: durations,
+          from: 0,
+          to: 1,
+          easing: "easeOutQuart"
+        };
+
+        data.element.animate(animations);
+      }
+    });
+
+    // chart.on("created", function () {
+    //   if (window.__exampleAnimateTimeout) {
+    //     clearTimeout(window.__exampleAnimateTimeout);
+    //     window.__exampleAnimateTimeout = null;
+    //   }
+    //   window.__exampleAnimateTimeout = setTimeout(
+    //     chart.update.bind(chart),
+    //     30000
+    //   );
+    // });
+  };
 
   return (
     <>
       {/* <!-- Panel line css animation Chart --> */}
-      <div className="panel">
-        <div className="panel-heading">
-          <h3 className="panel-title">CSS ANIMATION</h3>
-        </div>
-        <div className="panel-body">
-          <div className="row row-lg">
-            <div className="col-lg-6">
-              <div className="ct-chart1" style={{ height: "300px" }}></div>
-            </div>
-            <div className="col-lg-6">
-              <div className="ct-chart2" style={{ height: "300px" }}></div>
+      <div className="row">
+        <div className="col-xxl-6 col-md-12">
+          <div className="card card-shadow" id="chartThreeLinearea">
+            <div className="card-block p-0 h-full">
+              <div
+                className="pt-30 px-30"
+                style={{ height: "calc(100% - 250px);" }}
+              >
+                <div className="row">
+                  <div className="col-8">
+                    <p className="font-size-20 grey-700">CSS ANIMATION</p>
+                    <p>
+                      Quisque volutpat condimentum velit. Class aptent taciti
+                    </p>
+                  </div>
+
+                  <div className="col-4">
+                    <div className="float-right clearfix">
+                      <ul className="list-unstyled">
+                        <li className="mb-5">
+                          <i
+                            className="icon md-circle green-600 mr-5"
+                            aria-hidden="true"
+                          ></i>{" "}
+                          Diretary intake
+                        </li>
+                        <li className="mb-5">
+                          <i
+                            className="icon md-circle orange-600 mr-5"
+                            aria-hidden="true"
+                          ></i>{" "}
+                          Motion
+                        </li>
+                        <li className="mb-5">
+                          <i
+                            className="icon md-circle red-600 mr-5"
+                            aria-hidden="true"
+                          ></i>{" "}
+                          Other
+                        </li>
+                        <li className="mb-5">
+                          <i
+                            className="icon md-circle red-600 mr-5"
+                            aria-hidden="true"
+                          ></i>{" "}
+                          Other
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row row-lg">
+                <div className="col-lg-6">
+                  <div className="ct-chart1" style={{ height: "250px" }}></div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="ct-chart2" style={{ height: "250px" }}></div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
