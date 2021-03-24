6afade0e294ae5f96fafe0416e20ab98ccc23b09
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-25 02:02:42 +0900

    메인페이지 api 수정

--- api/views.py
+++ api/views.py
@@ -13,15 +13,25 @@ db = cluster["three-step"]
 
 @api_view(['GET'])
 def mainView(request):
-    main_api = db['main_api']
+    main_api = db['local_data_api']
 
-    main_api_data = main_api.find_one({"_id": ObjectId("604b258a82668c22d94e1d6d")})
+    main_data = main_api.find_one({"_id": ObjectId("60586c8dd8133683b6873f03")})
+    month_data = main_api.find_one({"_id": ObjectId("60593e8b4496e774152d0135")})
+    same_address_data = main_api.find_one({"_id": ObjectId("605950dbaa32a27df572863e")})
 
     api_json = {
         'title' : "Main View",
-        'indsLcls19' : main_api_data['indsLcls19'],
-        'indsLcls20' : main_api_data['indsLcls20'],
-        'ctprvnNm20' : main_api_data['ctprvnNm20'],
+        'type_close_19' : main_data['type_close_19'],
+        'type_open_19' : main_data['type_open_19'],
+        'type_close_20' : main_data['type_close_20'],
+        'type_open_20' : main_data['type_open_20'],
+        'city_close_20' : main_data['city_close_20'],
+        'city_close_20' : main_data['city_close_20'],
+        'month_close_19' : month_data['month_close_19'],
+        'month_open_19' : month_data['month_open_19'],
+        'month_close_20' : month_data['month_close_20'],
+        'month_open_20' : month_data['month_open_20'],
+        'nationwide_most_close_20' : same_address_data['nationwide']
     }
 
     return Response(api_json)
