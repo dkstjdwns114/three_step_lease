a1a98dedee15a930c8c79b0c2a10b4a5bc1c532d
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-24 23:45:07 +0900

    컴포넌트 이름 변경

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,9 +3,10 @@ import PrevPageHeader from "../components/Navigation/PrevPageHeader";
 import PrevPieChart from "../components/Chart/PrevPieChart";
 import PrevLoading from "../components/Loading/PrevLoading";
 import TestMap from "../components/Map/TestMap";
-import ChartistTest from "../components/Chart/ChartistMonth";
+import ChartistMonth from "../components/Chart/ChartistMonth";
 import CategoryChart from "../components/Chart/CategoryChart";
 import CityMap from "../components/Map/CityMap";
+import TabsAccording from "../components/Table/TabsAccording";
 
 export default class HomeTest extends Component {
   state = {
@@ -49,7 +50,7 @@ export default class HomeTest extends Component {
           <>
             <div className="page-content container-fluid">
               <div className="row" data-plugin="matchHeight" data-by-row="true">
-                <ChartistTest />
+                <ChartistMonth />
                 <CategoryChart />
                 <CityMap
                   cardTitle={"2020년 시도별 폐업 현황"}
@@ -57,6 +58,7 @@ export default class HomeTest extends Component {
                   cities={this.state.ctprvnNm20}
                   numberComma={this.numberWithCommas}
                 />
+                <TabsAccording />
               </div>
             </div>
           </>
