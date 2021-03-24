04f76f957cf95f060c9be0b26e354153ebbf7c93
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-24 23:42:32 +0900

    Header 테마 수정

--- frontend/src/components/Navigation/MainNavbar.js
+++ frontend/src/components/Navigation/MainNavbar.js
@@ -4,7 +4,7 @@ import { NavLink, Link } from "react-router-dom";
 const mainNavigation = (props) => {
   return (
     <nav
-      className="site-navbar navbar navbar-default navbar-fixed-top navbar-mega bg-cyan-600"
+      className="site-navbar navbar navbar-default navbar-fixed-top navbar-mega bg-cyan-600 navbar-inverse"
       role="navigation"
     >
       <div className="navbar-header">
