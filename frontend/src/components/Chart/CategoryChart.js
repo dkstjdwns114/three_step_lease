1ba281cdc3aaa8e172008e3ea301a99beb239d28
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-25 13:59:20 +0900

    업종별 차트 api로 출력 완료

--- frontend/src/components/Chart/CategoryChart.js
+++ frontend/src/components/Chart/CategoryChart.js
@@ -1,61 +1,90 @@
 import React, { useEffect, useState } from "react";
 import { Radar } from "react-chartjs-2";
 
-const data = {
-  labels: [
-    "Eating",
-    "Drinking",
-    "Sleeping",
-    "Designing",
-    "Coding",
-    "Cycling",
-    "Running"
-  ],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgba(220,220,220,0.2)",
-      pointBackgroundColor: "rgba(220,220,220,1)",
-      data: ["1", "2", "3", "4", "5", "6", "7"]
-    },
-    {
-      label: "Hidden dataset",
-      hidden: true,
-      data: ["7", "6", "5", "4", "3", "2", "1"]
-    },
-    {
-      label: "My Second dataset",
-      backgroundColor: "rgba(151,187,205,0.2)",
-      pointBackgroundColor: "rgba(151,187,205,1)",
-      hoverPointBackgroundColor: "#fff",
-      pointHighlightStroke: "rgba(151,187,205,1)",
-      data: ["4", "5", "6", "1", "2", "7", "20"]
-    }
-  ]
-};
+const CategoryChart = (props) => {
+  const [data, setData] = useState({});
+  const [options, setOptions] = useState({});
 
-const options = {
-  scale: {
-    reverse: false,
-    gridLines: {
-      color: [
-        "black",
-        "red",
-        "orange",
-        "yellow",
-        "green",
-        "blue",
-        "indigo",
-        "violet"
-      ]
-    },
-    ticks: {
-      beginAtZero: true
-    }
-  }
-};
+  useEffect(() => {
+    console.log(props);
+    setOptions({
+      scale: {
+        reverse: false,
+        gridLines: {
+          color: [
+            "black",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "indigo",
+            "violet"
+          ]
+        },
+        ticks: {
+          beginAtZero: true
+        }
+      }
+    });
+    let labelArr = [];
+    let datasetsArr = [];
 
-const categoryChart = (props) => {
+    let close_19_data = [];
+    let open_19_data = [];
+    let close_20_data = [];
+    let open_20_data = [];
+
+    props.type_close_19.forEach((data) => {
+      labelArr.push(data.type);
+      close_19_data.push(data.count);
+    });
+    props.type_open_19.forEach((data) => {
+      open_19_data.push(data.count);
+    });
+    props.type_close_20.forEach((data) => {
+      close_20_data.push(data.count);
+    });
+    props.type_open_20.forEach((data) => {
+      open_20_data.push(data.count);
+    });
+
+    setTimeout(() => {
+      datasetsArr.push({
+        label: "2019년 폐업",
+        backgroundColor: "rgba(220,220,220,0.2)",
+        pointBackgroundColor: "rgba(220,220,220,1)",
+        data: close_19_data
+      });
+      datasetsArr.push({
+        label: "2019년 개업",
+        hidden: true,
+        data: open_19_data
+      });
+      datasetsArr.push({
+        label: "2020년 폐업",
+        backgroundColor: "rgba(151,187,205,0.2)",
+        pointBackgroundColor: "rgba(151,187,205,1)",
+        hoverPointBackgroundColor: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: close_20_data
+      });
+      datasetsArr.push({
+        label: "2020년 개업",
+        hidden: true,
+        data: open_20_data
+      });
+      setDataAndOptions(labelArr, datasetsArr);
+    }, 500);
+  }, []);
+
+  const setDataAndOptions = (labelArr, datasetsArr) => {
+    setData({
+      labels: labelArr,
+      datasets: datasetsArr
+    });
+  };
+
   return (
     <>
       <div className="col-xxl-6 col-lg-6 col-md-12">
@@ -75,4 +104,4 @@ const categoryChart = (props) => {
   );
 };
 
-export default categoryChart;
+export default CategoryChart;
