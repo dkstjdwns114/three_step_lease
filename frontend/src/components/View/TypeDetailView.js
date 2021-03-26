bd24fabc07352d25f542438c1b7f9bf0184c39d4
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-27 02:16:35 +0900

    TypeDetailYearsBtn TypeDetailView에 합치기

--- frontend/src/components/View/TypeDetailView.js
+++ frontend/src/components/View/TypeDetailView.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import TypeDetailYearsBtn from "../Button/TypeDetailYearsBtn";
 import StandardTabs from "../Tab/StandardTabs";
 
 export default class TypeDetailView extends Component {
@@ -18,6 +17,20 @@ export default class TypeDetailView extends Component {
   changeYearsValue = (value) => {
     this.setState({ currentValue: value });
   };
+
+  close_19_click_handler = () => {
+    this.setState({ currentValue: "type_detail_close_19" });
+  };
+  close_20_click_handler = () => {
+    this.setState({ currentValue: "type_detail_close_20" });
+  };
+  open_19_click_handler = () => {
+    this.setState({ currentValue: "type_detail_open_19" });
+  };
+  open_20_click_handler = () => {
+    this.setState({ currentValue: "type_detail_open_20" });
+  };
+
   render() {
     return (
       <>
@@ -38,9 +51,73 @@ export default class TypeDetailView extends Component {
                     yearsValue={this.state.currentValue}
                   />
                 </div>
-                <TypeDetailYearsBtn
-                  buttonChangeHandler={this.changeYearsValue}
-                />
+                <p>
+                  To use justified button groups with{" "}
+                  <code>&lt;button&gt;</code> elements, you must wrap each
+                  button in a button group. Most browsers don't properly apply
+                  our CSS for justification to
+                  <code>&lt;button&gt;</code> elements, but since we support
+                  button dropdowns, we can work around that.
+                </p>
+                <div className="example example-buttons">
+                  <div className="btn-group btn-group-justified">
+                    <div className="btn-group" role="group">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={this.close_19_click_handler}
+                      >
+                        <i className="icon md-star" aria-hidden="true"></i>
+                        <br />
+                        <span className="text-uppercase hidden-sm-down">
+                          2019년도 폐업
+                        </span>
+                      </button>
+                    </div>
+
+                    <div className="btn-group" role="group">
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        onClick={this.open_19_click_handler}
+                      >
+                        <i className="icon md-time" aria-hidden="true"></i>
+                        <br />
+                        <span className="text-uppercase hidden-sm-down">
+                          2019년도 개업
+                        </span>
+                      </button>
+                    </div>
+
+                    <div className="btn-group" role="group">
+                      <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={this.close_20_click_handler}
+                      >
+                        <i className="icon md-account" aria-hidden="true"></i>
+                        <br />
+                        <span className="text-uppercase hidden-sm-down">
+                          2020년도 폐업
+                        </span>
+                      </button>
+                    </div>
+
+                    <div className="btn-group" role="group">
+                      <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={this.open_20_click_handler}
+                      >
+                        <i className="icon md-account" aria-hidden="true"></i>
+                        <br />
+                        <span className="text-uppercase hidden-sm-down">
+                          2020년도 개업
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
