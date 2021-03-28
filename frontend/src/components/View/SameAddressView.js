b0a419749e5b55e5cfe93e9fc84abc0d68bd3de5
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-28 21:48:10 +0900

    Same address Map Loading animation 완료

--- frontend/src/components/View/SameAddressView.js
+++ frontend/src/components/View/SameAddressView.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PrevLoading from "../Loading/PrevLoading";
+import RoundCircleLoading from "../Loading/RoundCircleLoading";
 import SameAddressMap from "../Map/SameAddressMap";
 
 export default class SameAddressView extends Component {
@@ -36,43 +37,40 @@ export default class SameAddressView extends Component {
   render() {
     return (
       <>
-        {this.state.isLoading ? (
-          <PrevLoading />
-        ) : (
-          <>
-            <div className="col-xxl-12 col-md-12">
-              <div className="card">
-                <div className="card-block p-0 h-full">
-                  <div
-                    className="pt-30 px-30"
-                    style={{ height: "calc(100% - 250px);" }}
-                  >
-                    <div className="row">
-                      <div className="col-8 pt-30 px-30">
-                        <p className="font-size-20 grey-700">CSS ANIMATION</p>
-                        <p>
-                          Quisque volutpat condimentum velit. Class aptent
-                          taciti
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row pt-30 px-30">
-                    <div className="col-lg-12">
-                      <SameAddressMap
-                        address={this.state.same_address}
-                        city_hoall_lat={this.props.city_hoall_lat}
-                        city_hoall_lng={this.props.city_hoall_lng}
-                        same_address_map_level={
-                          this.props.same_address_map_level
-                        }
-                      />
-                    </div>
+        <div className="col-xxl-12 col-md-12">
+          <div className="card">
+            <div className="card-block p-0 h-full">
+              <div
+                className="pt-30 px-30"
+                style={{ height: "calc(100% - 250px);" }}
+              >
+                <div className="row">
+                  <div className="col-8 pt-30 px-30">
+                    <p className="font-size-20 grey-700">CSS ANIMATION</p>
+                    <p>
+                      Quisque volutpat condimentum velit. Class aptent taciti
+                    </p>
                   </div>
                 </div>
               </div>
+              <div className="row pt-30 px-30">
+                <div className="col-lg-12">
+                  {this.state.isLoading ? (
+                    <RoundCircleLoading />
+                  ) : (
+                    <SameAddressMap
+                      address={this.state.same_address}
+                      city_hoall_lat={this.props.city_hoall_lat}
+                      city_hoall_lng={this.props.city_hoall_lng}
+                      same_address_map_level={this.props.same_address_map_level}
+                    />
+                  )}
+                </div>
+              </div>
             </div>
-            {/* <div className="content">
+          </div>
+        </div>
+        {/* <div className="content">
               <div className="row"></div>
               <PrevSameCoordinatesMap
                 cardTitle={"지도"}
@@ -82,8 +80,6 @@ export default class SameAddressView extends Component {
                 coordinates={this.state.same_coordinates}
               />
             </div> */}
-          </>
-        )}
       </>
     );
   }
