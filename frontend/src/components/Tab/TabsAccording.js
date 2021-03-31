1402710e48ab35992b08dac8701d4c6854681a3c
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-31 22:40:08 +0900

    텍스트 수정

--- frontend/src/components/Tab/TabsAccording.js
+++ frontend/src/components/Tab/TabsAccording.js
@@ -15,10 +15,9 @@ const TabsAccording = (props) => {
         <div className="card">
           <div className="card-block p-0 p-30 h-full">
             <div className="counter text-left">
-              <h4 className="font-size-16 grey-700">
-                {props.city_name} 2019년 2020년 모두 폐업 이력이 있는 주소 중
-                가게가 많은 주소 Top10
-              </h4>
+              <span className="counter-number">
+                {props.city_name} 2019년, 2020년 모두 폐업한 상가 주소 Top10
+              </span>
               <div
                 className="panel-group panel-group-continuous"
                 id="exampleAccordionContinuous"
