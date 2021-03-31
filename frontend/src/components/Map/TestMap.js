d86c46b0b6a6894e393a011dd12c6a2d5d405043
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-31 15:21:36 +0900

    경도 위도로 같은주소 지도에 표시 완료

--- frontend/src/components/Map/TestMap.js
+++ frontend/src/components/Map/TestMap.js
@@ -1,57 +1,52 @@
 import React, { useEffect } from "react";
 const { kakao } = window;
 
-const myAddr = [
-  "충청남도 당진시 동부1로 15, 당진농협 (대덕동)",
-  "강원도 원주시 라옹정길 55, 203호 (관설동)",
-  "대구광역시 수성구 지범로23길 34 (지산동)",
-  "경상남도 진주시 도동로232번길 14-1 (하대동)",
-  "부산광역시 사하구 낙동대로516번길 23 (하단동)"
-];
-
 const TestMap = (props) => {
-  useEffect(async () => {
+  useEffect(() => {
     const container = document.getElementById("map_marker_simple");
 
     let map = new kakao.maps.Map(container, {
-      center: new kakao.maps.LatLng(35.6783, 127.9558), // 지도의 중심좌표
-      level: 13
+      center: new kakao.maps.LatLng(props.city_hoall_lat, props.city_hoall_lng), // 지도의 중심좌표
+      level: props.same_address_map_level
     });
-
-    let geocoder = new kakao.maps.services.Geocoder();
 
     let clusterer = new kakao.maps.MarkerClusterer({
       map: map,
       averageCenter: true,
-      minLevel: 10
+      minLevel: 8,
+      disableClickZoom: true
     });
-    let coordinates = [];
-    await myAddr.forEach((address) => {
-      geocoder.addressSearch(address, (result, status) => {
-        if (status === kakao.maps.services.Status.OK) {
-          coordinates.push({ lat: result[0].y, lng: result[0].x });
+
+    let markers = props.same_address_list.map((coordinate, idx) => {
+      if (props.city_name === "전국") {
+        let stores_cnt = coordinate.stores_info.length;
+        if (stores_cnt < 70) {
+          return new kakao.maps.Marker({
+            text: coordinate.address,
+            position: new kakao.maps.LatLng(coordinate.lat, coordinate.lon)
+          });
         }
-      });
-    });
-    setTimeout(() => {
-      let markers = coordinates.map((coordinate) => {
-        console.log(coordinate);
+      } else {
         return new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(coordinate.lat, coordinate.lng)
+          text: coordinate.address,
+          position: new kakao.maps.LatLng(coordinate.lat, coordinate.lon)
         });
-      });
-      clusterer.addMarkers(markers);
-    }, [1000]);
-  }, []);
+      }
+    });
+
+    clusterer.addMarkers(markers);
+
+    kakao.maps.event.addListener(clusterer, "clusterclick", function (cluster) {
+      let level = map.getLevel() - 1;
+
+      map.setLevel(level, { anchor: cluster.getCenter() });
+    });
+  }, [props]);
 
   return (
     <>
       <div className="card">
-        <div className="card-header header-elements-inline">
-          <h5 className="card-title">{props.cardTitle}</h5>
-        </div>
         <div className="card-body">
-          <p className="mb-3">{props.cardDesc}</p>
           <div
             className="map-container"
             id="map_marker_simple"
