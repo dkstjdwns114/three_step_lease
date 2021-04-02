96e7af5d1afe0330d4b707aa15e9090d7b75c95e
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-03 00:10:10 +0900

    실시간데이터 업종 대분류 PolarChart  완료

--- frontend/src/components/Chart/RealTimePolarChart.js
+++ frontend/src/components/Chart/RealTimePolarChart.js
@@ -5,65 +5,41 @@ const RealTimePolarChart = (props) => {
   const [data, setData] = useState({});
 
   useEffect(() => {
+    let myLabels = [];
+    let myData = [];
+
+    props.data_list.forEach((info) => {
+      if (info.count !== 0) {
+        myLabels.push(info.type);
+        myData.push(info.count);
+      }
+    });
+    setDataAndOptions(myLabels, myData);
+  }, [props.data_list]);
+
+  const setDataAndOptions = (myLabels, myData) => {
     setData({
+      labels: myLabels,
       datasets: [
         {
-          data: [10, 20, 30]
+          backgroundColor: [
+            "#90CAF9",
+            "#E040FB",
+            "#84FFFF",
+            "#4DB6AC",
+            "#FFCDD2",
+            "#CDDC39",
+            "#F57C00",
+            "#A1887F",
+            "#69F0AE",
+            "#FFD180",
+            "#388E3C"
+          ],
+          data: myData
         }
-      ],
-      labels: ["Red", "Yellow", "Blue"]
+      ]
     });
-    //   let myLabels = [];
-    //   let myData = [];
-    //   if (props.yearsValue === "type_detail_close_19") {
-    //     props.close_19.type_list.forEach((list) => {
-    //       myLabels.push(list.kor_type);
-    //       myData.push(list.count);
-    //     });
-    //   } else if (props.yearsValue === "type_detail_open_19") {
-    //     props.open_19.type_list.forEach((list) => {
-    //       myLabels.push(list.kor_type);
-    //       myData.push(list.count);
-    //     });
-    //   } else if (props.yearsValue === "type_detail_close_20") {
-    //     props.close_20.type_list.forEach((list) => {
-    //       myLabels.push(list.kor_type);
-    //       myData.push(list.count);
-    //     });
-    //   } else if (props.yearsValue === "type_detail_open_20") {
-    //     props.open_20.type_list.forEach((list) => {
-    //       myLabels.push(list.kor_type);
-    //       myData.push(list.count);
-    //     });
-    //   }
-    //   setTimeout(() => {
-    //     setDataAndOptions(myLabels, myData);
-    //   }, 500);
-  }, [props.yearsValue]);
-
-  // const setDataAndOptions = (myLabels, myData) => {
-  //   setData({
-  //     labels: myLabels,
-  //     datasets: [
-  //       {
-  //         backgroundColor: [
-  //           "#90CAF9",
-  //           "#E040FB",
-  //           "#84FFFF",
-  //           "#4DB6AC",
-  //           "#FFCDD2",
-  //           "#CDDC39",
-  //           "#F57C00",
-  //           "#A1887F",
-  //           "#69F0AE",
-  //           "#FFD180",
-  //           "#388E3C"
-  //         ],
-  //         data: myData
-  //       }
-  //     ]
-  //   });
-  // };
+  };
 
   return <Polar data={data} />;
 };
--- frontend/src/components/View/RealTimeCategoryView.js
+++ frontend/src/components/View/RealTimeCategoryView.js
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import RealTimePolarChart from "../Chart/RealTimePolarChart";
 
 const RealTimeCategorylView = (props) => {
-  const [currentValue, setCurrentValue] = useState("");
+  const [currentDateValue, setCurrentDateValue] = useState("");
+  const [currentKorValue, setCurrentKorValue] = useState("");
+  const [openOrClose, setOpenOrClose] = useState("");
+  const [categoryData, setCategoryData] = useState([]);
   const [leftBtnActive, setLeftBtnActive] = useState(true);
   const [leftBtnClass, setLeftBtnClass] = useState(
     "btn btn-round btn-success btn-pill-left"
@@ -12,53 +15,146 @@ const RealTimeCategorylView = (props) => {
   );
 
   useEffect(() => {
-    setCurrentValue(props.one_days_ago_date);
+    setCurrentDateValue(props.one_days_ago_date);
+    setCurrentKorValue(props.one_days_ago_date_kor);
+    setOpenOrClose("폐업");
+    setCategoryData(props.one_days_ago.close.category);
   }, [props.one_days_ago_date]);
 
   const oneDaysChangeClickHandler = () => {
     const value = props.one_days_ago_date;
-    setCurrentValue(value);
+    const korValue = props.one_days_ago_date_kor;
+    setCurrentDateValue(value);
+    setCurrentKorValue(korValue);
+
+    if (openOrClose === "개업") {
+      setCategoryData(props.one_days_ago.open.category);
+    } else if (openOrClose === "폐업") {
+      setCategoryData(props.one_days_ago.close.category);
+    }
   };
 
   const twoDaysChangeClickHandler = () => {
     const value = props.two_days_ago_date;
-    setCurrentValue(value);
+    const korValue = props.two_days_ago_date_kor;
+    setCurrentDateValue(value);
+    setCurrentKorValue(korValue);
+
+    if (openOrClose === "개업") {
+      setCategoryData(props.two_days_ago.open.category);
+    } else if (openOrClose === "폐업") {
+      setCategoryData(props.two_days_ago.close.category);
+    }
   };
 
   const threeDaysChangeClickHandler = () => {
     const value = props.three_days_ago_date;
-    setCurrentValue(value);
+    const korValue = props.three_days_ago_date_kor;
+    setCurrentDateValue(value);
+    setCurrentKorValue(korValue);
+
+    if (openOrClose === "개업") {
+      setCategoryData(props.three_days_ago.open.category);
+    } else if (openOrClose === "폐업") {
+      setCategoryData(props.three_days_ago.close.category);
+    }
   };
 
   const fourDaysChangeClickHandler = () => {
     const value = props.four_days_ago_date;
-    setCurrentValue(value);
+    const korValue = props.four_days_ago_date_kor;
+    setCurrentDateValue(value);
+    setCurrentKorValue(korValue);
+
+    if (openOrClose === "개업") {
+      setCategoryData(props.four_days_ago.open.category);
+    } else if (openOrClose === "폐업") {
+      setCategoryData(props.four_days_ago.close.category);
+    }
   };
 
   const fiveDaysChangeClickHandler = () => {
     const value = props.five_days_ago_date;
-    setCurrentValue(value);
+    const korValue = props.five_days_ago_date_kor;
+    setCurrentDateValue(value);
+    setCurrentKorValue(korValue);
+
+    if (openOrClose === "개업") {
+      setCategoryData(props.five_days_ago.open.category);
+    } else if (openOrClose === "폐업") {
+      setCategoryData(props.five_days_ago.close.category);
+    }
   };
 
   const sixDaysChangeClickHandler = () => {
     const value = props.six_days_ago_date;
-    setCurrentValue(value);
+    const korValue = props.six_days_ago_date_kor;
+    setCurrentDateValue(value);
+    setCurrentKorValue(korValue);
+
+    if (openOrClose === "개업") {
+      setCategoryData(props.six_days_ago.open.category);
+    } else if (openOrClose === "폐업") {
+      setCategoryData(props.six_days_ago.close.category);
+    }
   };
 
   const sevenDaysChangeClickHandler = () => {
     const value = props.seven_days_ago_date;
-    setCurrentValue(value);
+    const korValue = props.seven_days_ago_date_kor;
+    setCurrentDateValue(value);
+    setCurrentKorValue(korValue);
+
+    if (openOrClose === "개업") {
+      setCategoryData(props.seven_days_ago.open.category);
+    } else if (openOrClose === "폐업") {
+      setCategoryData(props.seven_days_ago.close.category);
+    }
   };
 
   const btnClickHandler = () => {
     if (leftBtnActive) {
       setLeftBtnActive(false);
+      setOpenOrClose("개업");
       setLeftBtnClass("btn btn-round btn-default btn-pill-left");
       setRightBtnClass("btn btn-round btn-success btn-pill-right");
+
+      if (currentDateValue === props.one_days_ago_date) {
+        setCategoryData(props.one_days_ago.open.category);
+      } else if (currentDateValue === props.two_days_ago_date) {
+        setCategoryData(props.two_days_ago.open.category);
+      } else if (currentDateValue === props.three_days_ago_date) {
+        setCategoryData(props.three_days_ago.open.category);
+      } else if (currentDateValue === props.four_days_ago_date) {
+        setCategoryData(props.four_days_ago.open.category);
+      } else if (currentDateValue === props.five_days_ago_date) {
+        setCategoryData(props.five_days_ago.open.category);
+      } else if (currentDateValue === props.six_days_ago_date) {
+        setCategoryData(props.six_days_ago.open.category);
+      } else if (currentDateValue === props.seven_days_ago_date) {
+        setCategoryData(props.seven_days_ago.open.category);
+      }
     } else {
       setLeftBtnActive(true);
+      setOpenOrClose("폐업");
       setLeftBtnClass("btn btn-round btn-success btn-pill-left");
       setRightBtnClass("btn btn-round btn-default btn-pill-right");
+
+      if (currentDateValue === props.one_days_ago_date) {
+        setCategoryData(props.one_days_ago.close.category);
+      } else if (currentDateValue === props.two_days_ago_date) {
+        setCategoryData(props.two_days_ago.close.category);
+      } else if (currentDateValue === props.three_days_ago_date) {
+        setCategoryData(props.three_days_ago.close.category);
+      } else if (currentDateValue === props.four_days_ago_date) {
+        setCategoryData(props.four_days_ago.close.category);
+      } else if (currentDateValue === props.five_days_ago_date) {
+        setCategoryData(props.five_days_ago.close.category);
+      } else if (currentDateValue === props.six_days_ago_date) {
+        setCategoryData(props.six_days_ago.close.category);
+      } else if (currentDateValue === props.seven_days_ago_date) {
+        setCategoryData(props.seven_days_ago.close.category);
+      }
     }
   };
 
@@ -73,7 +169,7 @@ const RealTimeCategorylView = (props) => {
         >
           <i className="icon md-calendar" aria-hidden="true"></i>
           <span className="text-uppercase hidden-sm-down pb-5">
-            &nbsp;{currentValue}&nbsp;
+            &nbsp;{currentDateValue}&nbsp;
           </span>
         </div>
         <div className="dropdown-menu">
@@ -112,6 +208,9 @@ const RealTimeCategorylView = (props) => {
           </button>
         </div>
       </div>
+      <span>
+        &nbsp; {currentKorValue} {openOrClose}데이터
+      </span>
       <div className="float-right clearfix">
         <button
           type="button"
@@ -131,7 +230,7 @@ const RealTimeCategorylView = (props) => {
 
       <div className="row pt-30 px-30">
         <div className="col-lg-12 mb-50">
-          <RealTimePolarChart />
+          <RealTimePolarChart data_list={categoryData} />
         </div>
       </div>
     </>
