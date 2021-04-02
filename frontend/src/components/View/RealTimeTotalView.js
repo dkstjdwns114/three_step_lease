5dc0b8c61094832255f32c43eb2ae1f9a2518f1f
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-02 21:02:39 +0900

    실시간데이터 전체 합계 차트 뷰로 분리

---
+++ frontend/src/components/View/RealTimeTotalView.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import RealTimeBarChart from "../Chart/RealTimeBarChart";
+import DownLoadCsv from "../Csv/DownLoadCsv";
+
+const RealTimeTotalView = (props) => {
+  const [currentValue, setCurrentValue] = useState("");
+  useEffect(() => {
+    console.log("props", props);
+    setCurrentValue(props.one_days_ago_date);
+  }, [props.one_days_ago_date]);
+
+  const oneDaysChangeClickHandler = () => {
+    const value = props.one_days_ago_date;
+    setCurrentValue(value);
+  };
+
+  const twoDaysChangeClickHandler = () => {
+    const value = props.two_days_ago_date;
+    setCurrentValue(value);
+  };
+
+  const threeDaysChangeClickHandler = () => {
+    const value = props.three_days_ago_date;
+    setCurrentValue(value);
+  };
+
+  const fourDaysChangeClickHandler = () => {
+    const value = props.four_days_ago_date;
+    setCurrentValue(value);
+  };
+
+  const fiveDaysChangeClickHandler = () => {
+    const value = props.five_days_ago_date;
+    setCurrentValue(value);
+  };
+
+  const sixDaysChangeClickHandler = () => {
+    const value = props.six_days_ago_date;
+    setCurrentValue(value);
+  };
+
+  const sevenDaysChangeClickHandler = () => {
+    const value = props.seven_days_ago_date;
+    setCurrentValue(value);
+  };
+
+  return (
+    <>
+      <div className="btn-group" role="group">
+        <div
+          className="btn btn-info dropdown-toggle"
+          id="countIconDropdown"
+          data-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i className="icon md-calendar" aria-hidden="true"></i>
+          <span className="text-uppercase hidden-sm-down pb-5">
+            &nbsp;{currentValue}&nbsp;
+          </span>
+        </div>
+        <div className="dropdown-menu">
+          <button className="dropdown-item" onClick={oneDaysChangeClickHandler}>
+            {props.one_days_ago_date}
+          </button>
+          <button className="dropdown-item" onClick={twoDaysChangeClickHandler}>
+            {props.two_days_ago_date}
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={threeDaysChangeClickHandler}
+          >
+            {props.three_days_ago_date}
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={fourDaysChangeClickHandler}
+          >
+            {props.four_days_ago_date}
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={fiveDaysChangeClickHandler}
+          >
+            {props.five_days_ago_date}
+          </button>
+          <button className="dropdown-item" onClick={sixDaysChangeClickHandler}>
+            {props.six_days_ago_date}
+          </button>
+          <button
+            className="dropdown-item"
+            onClick={sevenDaysChangeClickHandler}
+          >
+            {props.seven_days_ago_date}
+          </button>
+        </div>
+      </div>
+      <span>&nbsp; 데이터 다운로드 다운로드 다운로드</span>
+      <div className="row pt-30 px-30">
+        <div className="col-lg-12">
+          <RealTimeBarChart
+            one_days_ago={props.one_days_ago}
+            two_days_ago={props.two_days_ago}
+            three_days_ago={props.three_days_ago}
+            four_days_ago={props.four_days_ago}
+            five_days_ago={props.five_days_ago}
+            six_days_ago={props.six_days_ago}
+            seven_days_ago={props.seven_days_ago}
+            one_days_ago_date={props.one_days_ago_date}
+            two_days_ago_date={props.two_days_ago_date}
+            three_days_ago_date={props.three_days_ago_date}
+            four_days_ago_date={props.four_days_ago_date}
+            five_days_ago_date={props.five_days_ago_date}
+            six_days_ago_date={props.six_days_ago_date}
+            seven_days_ago_date={props.seven_days_ago_date}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RealTimeTotalView;
--- frontend/src/components/View/RealTimeView.js
+++ frontend/src/components/View/RealTimeView.js
@@ -3,6 +3,7 @@ import RealTimeBarChart from "../Chart/RealTimeBarChart";
 import RealTimePolarChart from "../Chart/RealTimePolarChart";
 import DownLoadCsv from "../Csv/DownLoadCsv";
 import RoundCircleLoading from "../Loading/RoundCircleLoading";
+import RealTimeTotalView from "./RealTimeTotalView";
 
 export default class RealTimeView extends Component {
   state = {
@@ -130,74 +131,10 @@ export default class RealTimeView extends Component {
                       {this.props.city_name} 최근 7일 개·폐업 현황
                     </p>
                     <p>매일 오전 9시에 전날 데이터가 업데이트 됩니다.</p>
-                    <div className="btn-group" role="group">
-                      <div
-                        className="btn btn-info dropdown-toggle"
-                        id="countIconDropdown"
-                        data-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        <i className="icon md-calendar" aria-hidden="true"></i>
-                        <span className="text-uppercase hidden-sm-down pb-5">
-                          &nbsp;{this.state.total_current_date_value}&nbsp;
-                        </span>
-                      </div>
-                      <div className="dropdown-menu">
-                        <button
-                          className="dropdown-item"
-                          onClick={this.oneDaysChangeClickHandler}
-                        >
-                          {this.state.one_days_ago_date}
-                        </button>
-                        <button
-                          className="dropdown-item"
-                          onClick={this.twoDaysChangeClickHandler}
-                        >
-                          {this.state.two_days_ago_date}
-                        </button>
-                        <button
-                          className="dropdown-item"
-                          onClick={this.threeDaysChangeClickHandler}
-                        >
-                          {this.state.three_days_ago_date}
-                        </button>
-                        <button
-                          className="dropdown-item"
-                          onClick={this.fourDaysChangeClickHandler}
-                        >
-                          {this.state.four_days_ago_date}
-                        </button>
-                        <button
-                          className="dropdown-item"
-                          onClick={this.fiveDaysChangeClickHandler}
-                        >
-                          {this.state.five_days_ago_date}
-                        </button>
-                        <button
-                          className="dropdown-item"
-                          onClick={this.sixDaysChangeClickHandler}
-                        >
-                          {this.state.six_days_ago_date}
-                        </button>
-                        <button
-                          className="dropdown-item"
-                          onClick={this.sevenDaysChangeClickHandler}
-                        >
-                          {this.state.seven_days_ago_date}
-                        </button>
-                      </div>
-                    </div>
-                    <span>&nbsp; 데이터 다운로드 다운로드 다운로드</span>
-                  </div>
-                </div>
-              </div>
-              <div className="row pt-30 px-30">
-                <div className="col-lg-12">
-                  {this.state.isLoading ? (
-                    <RoundCircleLoading />
-                  ) : (
-                    <>
-                      <RealTimeBarChart
+                    {this.state.isLoading ? (
+                      <RoundCircleLoading />
+                    ) : (
+                      <RealTimeTotalView
                         one_days_ago={this.state.one_days_ago}
                         two_days_ago={this.state.two_days_ago}
                         three_days_ago={this.state.three_days_ago}
@@ -213,8 +150,8 @@ export default class RealTimeView extends Component {
                         six_days_ago_date={this.state.six_days_ago_date}
                         seven_days_ago_date={this.state.seven_days_ago_date}
                       />
-                    </>
-                  )}
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
