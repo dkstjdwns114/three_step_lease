8553d89b7c01318b20122761e4cfdf6c7af143d7
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-02 14:34:02 +0900

    불필요한 코드 삭제

--- frontend/src/components/Chart/RealTimeBarChart.js
+++ frontend/src/components/Chart/RealTimeBarChart.js
@@ -1,55 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
 
-const chartOptions = {
-  scales: {
-    xAxes: [
-      {
-        barPercentage: 1,
-        categoryPercentage: 0.6
-      }
-    ],
-    yAxes: [
-      {
-        id: "y-axis-density"
-      },
-      {
-        id: "y-axis-gravity"
-      }
-    ]
-  }
-};
-
-// const densityData = {
-//   label: "Density of Planet (kg/m3)",
-//   data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638],
-//   backgroundColor: "rgba(0, 99, 132, 0.6)",
-//   borderColor: "rgba(0, 99, 132, 1)",
-//   yAxisID: "y-axis-density"
-// };
-
-// const gravityData = {
-//   label: "Gravity of Planet (m/s2)",
-//   data: [3.7, 8.9, 9.8, 3.7, 23.1, 9.0, 8.7, 11.0],
-//   backgroundColor: "rgba(99, 132, 0, 0.6)",
-//   borderColor: "rgba(99, 132, 0, 1)",
-//   yAxisID: "y-axis-gravity"
-// };
-
-// const planetData = {
-//   labels: [
-//     "Mercury",
-//     "Venus",
-//     "Earth",
-//     "Mars",
-//     "Jupiter",
-//     "Saturn",
-//     "Uranus",
-//     "Neptune"
-//   ],
-//   datasets: [densityData, gravityData]
-// };
-
 const RealTimeBarChart = (props) => {
   const [resultData, setResultData] = useState({});
 
@@ -77,7 +28,7 @@ const RealTimeBarChart = (props) => {
     const seven_days_ago = props.seven_days_ago.open.total;
 
     return {
-      label: "최근 7일 개업 현황",
+      label: "개업",
       data: [
         seven_days_ago,
         six_days_ago,
@@ -88,8 +39,7 @@ const RealTimeBarChart = (props) => {
         one_days_ago
       ],
       backgroundColor: "rgba(0, 99, 132, 0.6)",
-      borderColor: "rgba(0, 99, 132, 1)",
-      yAxisID: "y-axis-density"
+      borderColor: "rgba(0, 99, 132, 1)"
     };
   };
 
@@ -103,7 +53,7 @@ const RealTimeBarChart = (props) => {
     const seven_days_ago = props.seven_days_ago.close.total;
 
     return {
-      label: "최근 7일 폐업 현황",
+      label: "폐업",
       data: [
         seven_days_ago,
         six_days_ago,
@@ -115,8 +65,7 @@ const RealTimeBarChart = (props) => {
       ],
       backgroundColor: "rgba(0, 99, 132, 0.6)",
       backgroundColor: "rgba(99, 132, 0, 0.6)",
-      borderColor: "rgba(99, 132, 0, 1)",
-      yAxisID: "y-axis-gravity"
+      borderColor: "rgba(99, 132, 0, 1)"
     };
   };
 
@@ -148,7 +97,7 @@ const RealTimeBarChart = (props) => {
   };
   return (
     <>
-      <Bar data={resultData} options={chartOptions} />
+      <Bar data={resultData} />
     </>
   );
 };
