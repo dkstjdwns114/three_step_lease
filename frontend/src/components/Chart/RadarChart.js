851df00cefe21676bfa389c3d4f40c78b0ec0860
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-04 01:31:59 +0900

    업종 대분류 레이더 그래프 tooltip custom 완료

--- frontend/src/components/Chart/RadarChart.js
+++ frontend/src/components/Chart/RadarChart.js
@@ -6,26 +6,6 @@ const RadarChart = (props) => {
   const [options, setOptions] = useState({});
 
   useEffect(() => {
-    setOptions({
-      scale: {
-        reverse: false,
-        gridLines: {
-          color: [
-            "#424242",
-            "#F50057",
-            "#9FA8DA",
-            "#64B5F6",
-            "#FF7043",
-            "#AED581",
-            "#B3E5FC",
-            "#F06292",
-            "#42A5F5",
-            "#9C27B0",
-            "#FF1744"
-          ]
-        }
-      }
-    });
     let labelArr = [];
     let datasetsArr = [];
     let close_19_data = [];
@@ -49,16 +29,16 @@ const RadarChart = (props) => {
     datasetsArr.push({
       label: "2019년 폐업",
       backgroundColor: "rgba(238, 255, 65, 0.2)",
-      pointBackgroundColor: "rgba(238, 255, 65, 1)",
-      pointHighlightStroke: "rgba(238, 255, 65, 1)",
+      pointBackgroundColor: "rgba(238, 255, 65, 0.5)",
+      pointHighlightStroke: "rgba(238, 255, 65, 0.5)",
       hoverPointBackgroundColor: "#fff",
       data: close_19_data
     });
     datasetsArr.push({
       label: "2020년 폐업",
       backgroundColor: "rgba(240, 98, 146, 0.2)",
-      pointBackgroundColor: "rgba(240, 98, 146, 1)",
-      pointHighlightStroke: "rgba(240, 98, 146, 1)",
+      pointBackgroundColor: "rgba(240, 98, 146, 0.5)",
+      pointHighlightStroke: "rgba(240, 98, 146, 0.5)",
       hoverPointBackgroundColor: "#fff",
       data: close_20_data
     });
@@ -66,8 +46,8 @@ const RadarChart = (props) => {
       label: "2019년 개업",
       hidden: true,
       backgroundColor: "rgba(105, 240, 174, 0.2)",
-      pointBackgroundColor: "rgba(105, 240, 174, 1)",
-      pointHighlightStroke: "rgba(105, 240, 174, 1)",
+      pointBackgroundColor: "rgba(105, 240, 174, 0.5)",
+      pointHighlightStroke: "rgba(105, 240, 174, 0.5)",
       hoverPointBackgroundColor: "#fff",
       data: open_19_data
     });
@@ -75,8 +55,8 @@ const RadarChart = (props) => {
       label: "2020년 개업",
       hidden: true,
       backgroundColor: "rgba(100, 181, 246, 0.2)",
-      pointBackgroundColor: "rgba(100, 181, 246, 1)",
-      pointHighlightStroke: "rgba(100, 181, 246, 1)",
+      pointBackgroundColor: "rgba(100, 181, 246, 0.5)",
+      pointHighlightStroke: "rgba(100, 181, 246, 0.5)",
       hoverPointBackgroundColor: "#fff",
       data: open_20_data
     });
@@ -87,6 +67,48 @@ const RadarChart = (props) => {
     setData({
       labels: labelArr,
       datasets: datasetsArr
+    });
+    setOptions({
+      scale: {
+        reverse: false,
+        gridLines: {
+          color: [
+            "#424242",
+            "#F50057",
+            "#9FA8DA",
+            "#64B5F6",
+            "#FF7043",
+            "#AED581",
+            "#B3E5FC",
+            "#F06292",
+            "#42A5F5",
+            "#9C27B0",
+            "#FF1744"
+          ]
+        }
+      },
+      tooltips: {
+        callbacks: {
+          title: (context) => {
+            let title = labelArr[context[0].index];
+            return title;
+          },
+          label: (context) => {
+            let label = " ";
+            if (context.datasetIndex === 0) {
+              label += "2019년 폐업: ";
+            } else if (context.datasetIndex === 1) {
+              label += "2020년 폐업: ";
+            } else if (context.datasetIndex === 2) {
+              label += "2019년 개업: ";
+            } else if (context.datasetIndex === 3) {
+              label += "2020년 개업: ";
+            }
+            label += props.numberWithCommas(context.yLabel) + "개";
+            return label;
+          }
+        }
+      }
     });
   };
   return (
