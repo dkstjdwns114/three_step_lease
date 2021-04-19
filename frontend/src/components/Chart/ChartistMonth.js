3dc53db5b606f0f467eb2a47502c924bac3efbc8
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-19 16:48:09 +0900

    불필요한 코드 삭제

--- frontend/src/components/Chart/ChartistMonth.js
+++ frontend/src/components/Chart/ChartistMonth.js
@@ -4,44 +4,6 @@ import ChartistTooltip from "chartist-plugin-tooltips-updated";
 
 const ChartistTest = (props) => {
   useEffect(() => {
-    // mobile
-    // let close_first_19 = [];
-    // let open_first_19 = [];
-    // let close_first_20 = [];
-    // let open_first_20 = [];
-
-    // let close_latter_19 = [];
-    // let open_latter_19 = [];
-    // let close_latter_20 = [];
-    // let open_latter_20 = [];
-
-    // let first_month = [];
-    // let latter_month = [];
-
-    // props.month_close_19.forEach((data) => {
-    //   data.month_num <= 6
-    //     ? (close_first_19.push({ meta: "2019년 폐업", value: data.count }),
-    //       first_month.push(data.month))
-    //     : (close_latter_19.push({ meta: "2019년 폐업", value: data.count }),
-    //       latter_month.push(data.month));
-    // });
-    // props.month_open_19.forEach((data) => {
-    //   data.month_num <= 6
-    //     ? open_first_19.push({ meta: "2019년 개업", value: data.count })
-    //     : open_latter_19.push({ meta: "2019년 개업", value: data.count });
-    // });
-    // props.month_close_20.forEach((data) => {
-    //   data.month_num <= 6
-    //     ? close_first_20.push({ meta: "2020년 폐업", value: data.count })
-    //     : close_latter_20.push({ meta: "2020년 폐업", value: data.count });
-    // });
-    // props.month_open_20.forEach((data) => {
-    //   data.month_num <= 6
-    //     ? open_first_20.push({ meta: "2020년 개업", value: data.count })
-    //     : open_latter_20.push({ meta: "2020년 개업", value: data.count });
-    // });
-
-    // web
     let close_19 = [];
     let open_19 = [];
     let close_20 = [];
@@ -65,14 +27,6 @@ const ChartistTest = (props) => {
 
     setTimeout(() => {
       animationChart(".ct-chart1", month, close_19, open_19, close_20, open_20);
-      // animationChart(
-      //   ".ct-chart2",
-      //   latter_month,
-      //   close_latter_19,
-      //   open_latter_19,
-      //   close_latter_20,
-      //   open_latter_20
-      // );
     }, 500);
   }, []);
 
@@ -191,30 +145,11 @@ const ChartistTest = (props) => {
         data.element.animate(animations);
       }
     });
-
-    // 애니메이션 반복
-    // chart.on("created", function () {
-    //   if (window.__exampleAnimateTimeout) {
-    //     clearTimeout(window.__exampleAnimateTimeout);
-    //     window.__exampleAnimateTimeout = null;
-    //   }
-    //   window.__exampleAnimateTimeout = setTimeout(
-    //     chart.update.bind(chart),
-    //     30000
-    //   );
-    // });
   };
 
   return (
     <>
-      {/* mobile은 col-lg-6으로 변경 */}
       <div className="ct-chart1" style={{ height: "500px" }}></div>
-      {/* mobile */}
-      {/* 
-          <div className="col-lg-6">
-            <div className="ct-chart2" style={{ height: "300px" }}></div>
-          </div>
-      */}
     </>
   );
 };
